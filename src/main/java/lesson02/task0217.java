8c93ae4b7325beb9e704c89c169f00a1efd82542
shklyaev0101 <ds.shklyaev@gmail.com>
2022-10-26 00:55:39 +0300

    вторая загрузка имеющихся файлов)

---
+++ src/main/java/lesson02/task0217.java
@@ -0,0 +1,27 @@
+package lesson02;
+
+/*
+Повторенье-мать
+Реализуй метод print3. Метод должен вывести на экран переданную строку 3 раза.
+Каждый раз с новой строки.
+
+Requirements:
+1. Программа должна выводить текст на экран.
+2. Метод main не должен вызывать функцию System.out.println или System.out.print.
+3. Метод print3 должен выводить текст на экран.
+4. Метод main должен вызвать метод print3 только один раз.
+5. Метод print3 должен выводить на экран строку 3 раза. Каждый раз с новой строки.
+ */
+
+public class task0217 {
+    public static void print3(String text) {
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+}
