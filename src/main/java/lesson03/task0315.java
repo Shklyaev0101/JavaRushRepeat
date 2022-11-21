c0a467f23802c459b38498ad0975d96e413e97ea
shklyaev0101 <ds.shklyaev@gmail.com>
2022-11-21 23:55:33 +0300

    загрузка имеющихся файлов (за 21.11.2022)

---
+++ src/main/java/lesson03/task0315.java
@@ -0,0 +1,40 @@
+package lesson03;
+
+/*
+План по захвату мира
+Ввести с клавиатуры число и имя, вывести на экран строку:
+"имя" захватит мир через "число" лет. Му-ха-ха!
+
+Пример:
+Вася захватит мир через 8 лет. Му-ха-ха!
+
+Последовательность вводимых данных имеет большое значение.
+
+
+Requirements:
+1. Программа должна выводить текст.
+2. Программа должна считывать данные с клавиатуры.
+3. Выведенный текст должен содержать введенное имя.
+4. Выведенный текст должен содержать введенное число.
+5. Выведенный текст должен полностью соответствовать заданию.
+ */
+
+// import ...
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class task0315 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String sAge = reader.readLine();
+        int nAge = Integer.parseInt(sAge);
+        String name = reader.readLine();
+
+
+        System.out.println(name + " захватит мир через " + nAge + " лет. Му ха-ха!");
+
+    }
+}
