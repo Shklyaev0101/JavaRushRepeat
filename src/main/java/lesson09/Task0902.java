918eab627883375498f28d0b86e93c9311d9d00f
shklyaev0101 <ds.shklyaev@gmail.com>
2023-02-21 00:52:48 +0300

    loading completed tasks (20.02.2023)

---
+++ src/main/java/lesson09/Task0902.java
@@ -0,0 +1,54 @@
+package lesson09;
+
+/**
+ * И снова StackTrace
+ *
+ * Написать пять методов, которые вызывают друг друга.
+ * Каждый метод должен возвращать имя метода, вызвавшего его, полученное с помощью StackTrace.
+ *
+ * Требования:
+ * •	В классе должно быть 5 методов (метод main не учитывать).
+ * •	Каждый метод должен вызывать следующий: main вызывать method1, method1 вызывать method2 и т.д.
+ * •	Каждый метод должен возвращать имя метода, вызвавшего его.
+ * •	Для получения имени вызвавшего метода, используй метод getMethodName.
+ */
+
+public class Task0902 {
+    public static void main(String[] args) {
+        method1();
+    }
+
+    public static String method1() {
+        method2();
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+    }
+
+    public static String method2() {
+        method3();
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+    }
+
+    public static String method3() {
+        method4();
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+    }
+
+    public static String method4() {
+        method5();
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+    }
+
+    public static String method5() {
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+    }
+}
