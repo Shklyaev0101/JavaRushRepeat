b8657f5ed4b63ecfe69dc330241c1b7f2122654c
shklyaev0101 <ds.shklyaev@gmail.com>
2023-03-16 22:07:48 +0300

    loading completed tasks (15.03.2023)

---
+++ src/main/java/lesson11/Task1110.java
@@ -0,0 +1,48 @@
+package lesson11;
+
+/**
+ * Не забываем инкапсулировать
+ *
+ * Посмотри внимательно на методы и добавь недостающие поля.
+ *
+ * Требования:
+ * •	В классе Cat должно быть поле типа String с именем name.
+ * •	В классе Cat должно быть поле типа int с именем age.
+ * •	В классе Cat должно быть поле типа int с именем weight.
+ * •	В классе Cat должно быть поле типа int с именем speed.
+ * •	В классе Cat должны быть 4 поля.
+ * •	Все поля класса Cat должны быть private.
+ */
+
+public class Task1110 {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight) {
+
+        }
+
+        public String getName() {
+            return null;
+        }
+
+        public int getAge() {
+            return 0;
+        }
+
+        public void setWeight(int weight) {
+
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+    }
+}
---
+++ src/main/java/lesson11/Task1113.java
@@ -0,0 +1,39 @@
+package lesson11;
+
+/**
+ * Играем в дарвина
+ *
+ * Измени четыре класса: Fish (Рыба), Animal (Животное), Ape (Обезьяна), Human (Человек).
+ * Унаследовать животное от рыбы, обезьяну — от животного и человека от обезьяны.
+ *
+ * Требования:
+ * •	В классе Solution должен быть public класс Fish (Рыба).
+ * •	В классе Solution должен быть public класс Animal (Животное).
+ * •	В классе Solution должен быть public класс Ape (Обезьяна).
+ * •	В классе Solution должен быть public класс Human (Человек).
+ * •	Класс Animal должен быть унаследован от класса Fish.
+ * •	Класс Ape должен быть унаследован от класса Animal.
+ * •	Класс Human должен быть унаследован от класса Ape.
+ */
+
+public class Task1113 {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+
+}
