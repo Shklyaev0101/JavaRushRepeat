6155cd37bca0eb92085b9546af7794487a1de100
shklyaev0101 <ds.shklyaev@gmail.com>
2023-03-18 00:37:13 +0300

    loading completed tasks (17.03.2023)

---
+++ src/main/java/lesson11/task1119.java
@@ -0,0 +1,38 @@
+package lesson11;
+
+/**
+ * Четвертая правильная «цепочка наследования»
+ *
+ * Расставь правильно «цепочку наследования» в классах: House (дом), Cat (кот), Dog (собака), Car (машина).
+ *
+ * Требования:
+ * •	В классе Solution должен быть public класс House (дом).
+ * •	В классе Solution должен быть public класс Cat (кот).
+ * •	В классе Solution должен быть public класс Dog (собака).
+ * •	В классе Solution должен быть public класс Car (машина).
+ * •	Правильно унаследуй класс House.
+ * •	Правильно унаследуй класс Cat.
+ * •	Правильно унаследуй класс Dog.
+ * •	Правильно унаследуй класс Car.
+ */
+
+public class task1119 {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+}
