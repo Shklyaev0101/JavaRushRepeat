ae8cb899aa744a25eeaccbe463865cc58505cdc1
shklyaev0101 <ds.shklyaev@gmail.com>
2023-03-18 23:54:14 +0300

    loading completed tasks (18.03.2023)

---
+++ src/main/java/lesson11/Task1124.java
@@ -0,0 +1,39 @@
+package lesson11;
+
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * Киты и коровы
+ *
+ * Переопредели метод getName в классе Whale(Кит), чтобы программа выдавала:
+ *
+ * Я не корова, Я - кит.
+ *
+ * Требования:
+ * •	Программа должна выводить текст на экран.
+ * •	Класс Whale должен наследоваться от класса Cow.
+ * •	Класс Whale должен переопределять метод getName().
+ * •	Метод main() должен вызывать метод getName() у объекта cow.
+ * •	Переопределить метод getName в классе Whale(Кит), чтобы программа выводила на экран: "Я не корова, Я - кит."
+ * •	Метод main() должен выводить на экран результат вызова метода getName().
+ */
+
+public class Task1124 {
+    public static void main(String[] args) {
+        Cow cow = new Whale();
+
+        System.out.println(cow.getName());
+    }
+
+    public static class Cow {
+        public String getName() {
+            return "Я - корова";
+        }
+    }
+
+    public static class Whale extends Cow {
+        public String getName() {
+            return "Я не корова, Я - кит.";
+        }
+    }
+}
